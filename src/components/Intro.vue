502998e3e3ff29e4f9aea8000dd948b949b23144
Antonio Molinari <antoniomolinari@me.com>
2025-11-05 19:54:50 +0100

    project setup and 1st tests

---
+++ src/components/Intro.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { onMounted } from 'vue';
+import Work from './Work.vue';
+
+
+onMounted(() => {
+  console.log('Intro component mounted.');
+});
+
+const works = [
+  { id: 1, title: 'Work 1' },
+  { id: 2, title: 'Work 2' },
+  { id: 3, title: 'Work 3' },
+  { id: 4, title: 'Work 4' },
+  { id: 5, title: 'Work 5' }
+
+
+]
+
+
+
+</script>
+
+<template>
+  <div class="title">
+    <h1>Welcome to the Intro Component!</h1>
+    <p>This is a simple introduction component.</p>
+  </div>
+  <div v-for="work in works" :key="work.id">
+    <Work :title="work.title" :description="work.description" />
+  </div>
+</template>
+
+<style scoped>
+  .title {
+    text-align: center;
+    color: #333;
+
+  }
+</style>
---
+++ src/components/Work.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  title: String,
+  description: String
+});
+</script>
+
+<template>
+  <div class="work">
+    <h2>{{ title }}</h2>
+    <p>{{ description}}.</p>
+  </div>
+</template>
